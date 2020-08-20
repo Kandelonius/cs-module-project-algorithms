204b6476d303928956750d33dbe766fe8a96f848
Shane Kaestner <shane.kaestner@yahoo.com>
2020-08-19 23:59:49 -0700

    I got the cookie problem done for large values.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,24 +2,31 @@
 Input: an integer
 Returns: an integer
 '''
-dict = {0: 1, 1: 1}
-
+dict = {0: 1, 1: 1, 2: 2}
 def eating_cookies(n):
-    global count
-    pass
+    # global count
     # create a dictionary to hold values that I know so I don't need to move down
     # each tree if I've already been down a tree of that number.
     ## contains then add that number to count
-    if dict.__contains__(n):
-        return dict[n]
-    ## if n is 0
-    else:
-        count = 1
-        count += eating_cookies(n - 1)
-        dict[n] = count
-        return dict[n]
-if __name__ == "__main__":
-    # Use the main function here to test out your implementation
-    num_cookies = 5
+    # if dict.__contains__(n):
+    #     return dict[n]
+    # ## if n is 0
+    # else:
+    #     count = 1
+    #     count += eating_cookies(n - 1)
+    #     dict[n] = count
+    #     return dict[n]
+    ### trying something based on pattern recognition and not figuring out the process
+    ### it appears eating_cookies(n) is a summation of the previous 3 options
+    index = 3
+    for thing in range(index, n + 1):
+        dict[thing] = dict[index - 1] + dict[index - 2] + dict[index - 3]
+        index += 1
+    return dict[n]
 
+        # Use the main function here to test out your implementation
+
+
+if __name__ == "__main__":
+    num_cookies = 6
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
